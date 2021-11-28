1d605e563964ac6aaddfa167dc8f9258009708c7
Vitalii Ismagilov <ismagilov.vel@gmail.com>
2021-11-28 23:24:25 +0200

    Get title, URL, source, quite(), close()

--- src/main/java/FirstTests.java
+++ src/main/java/FirstTests.java
@@ -1,3 +1,4 @@
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
@@ -16,35 +17,47 @@ public class FirstTests {
         WebDriver driver = new ChromeDriver();
         driver.get("http://google.com/");
 
-        // Get current URL
-        String currentUrl = driver.getCurrentUrl();
-        System.out.println(currentUrl);
+        // Get current URL + assertion
+        //String currentUrl = driver.getCurrentUrl();
+        //System.out.println(currentUrl);
+        //Assert.assertEquals(currentUrl, "https://www.google.com/?gws_rd=ssl");
+
+        // Get current title + assertion
+        //String title = driver.getTitle();
+        //System.out.println(title);
+        //Assert.assertEquals(title, "Google");
+
+        // Get page source
+        //String pageS = driver.getPageSource();
+        //System.out.println(pageS);
+
 
         // Almost the same as get() but with different options
         //driver.navigate().to("http://google.com/");
 
         /**
          * Switching between window tabs:
-        // Saving window id to Set
-        Set<String> oldWindow = driver.getWindowHandles();
-        System.out.println("oldWindow" + oldWindow);
+         // Saving window id to Set
+         Set<String> oldWindow = driver.getWindowHandles();
+         System.out.println("oldWindow" + oldWindow);
 
-        // Create new window via js
-        ((JavascriptExecutor)driver).executeScript("window.open()");
-        ArrayList<String> tabs = new ArrayList<>(driver.getWindowHandles());
+         // Create new window via js
+         ((JavascriptExecutor)driver).executeScript("window.open()");
+         ArrayList<String> tabs = new ArrayList<>(driver.getWindowHandles());
 
-        // Switching to new window tab with index 1
-        driver.switchTo().window(tabs.get(1));
-        driver.get("https://selenium.dev/");
+         // Switching to new window tab with index 1
+         driver.switchTo().window(tabs.get(1));
+         driver.get("https://selenium.dev/");
 
-        // Saving new set with window ids
-        Set<String> newWindow = driver.getWindowHandles();
-        System.out.println("newWindow" + newWindow);
+         // Saving new set with window ids
+         Set<String> newWindow = driver.getWindowHandles();
+         System.out.println("newWindow" + newWindow);
          */
 
         WebElement searchBox = driver.findElement(By.name("q"));
         searchBox.sendKeys("ChromeDriver");
         searchBox.submit();
-        driver.quit();
+        driver.quit(); // close all tabs and quite driver
+        //driver.close(); // close last tab only, in case there was only one tab - close driver as well
     }
 }
