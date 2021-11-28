563b08c82c632e1734827a1f3c646ac3214ab650
Vitalii Ismagilov <ismagilov.vel@gmail.com>
2021-11-28 22:15:18 +0200

    Window handles

--- src/main/java/FirstTests.java
+++ src/main/java/FirstTests.java
@@ -1,8 +1,12 @@
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.ArrayList;
+import java.util.Set;
 
 public class FirstTests {
 
@@ -11,6 +15,27 @@ public class FirstTests {
         System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
         WebDriver driver = new ChromeDriver();
         driver.get("http://google.com/");
+        //driver.navigate().to("http://google.com/");
+
+        /**
+         * Switching between window tabs:
+        // Saving window id to Set
+        Set<String> oldWindow = driver.getWindowHandles();
+        System.out.println("oldWindow" + oldWindow);
+
+        // Create new window via js
+        ((JavascriptExecutor)driver).executeScript("window.open()");
+        ArrayList<String> tabs = new ArrayList<>(driver.getWindowHandles());
+
+        // Switching to new window tab with index 1
+        driver.switchTo().window(tabs.get(1));
+        driver.get("https://selenium.dev/");
+
+        // Saving new set with window ids
+        Set<String> newWindow = driver.getWindowHandles();
+        System.out.println("newWindow" + newWindow);
+         */
+
         WebElement searchBox = driver.findElement(By.name("q"));
         searchBox.sendKeys("ChromeDriver");
         searchBox.submit();
