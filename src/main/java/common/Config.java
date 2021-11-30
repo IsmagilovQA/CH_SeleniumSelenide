6cfe2cf16723c23bfaf2edc5238b82e2bcf3a950
Vitalii Ismagilov <ismagilov.vel@gmail.com>
2021-11-30 19:41:48 +0200

    Configuring driver in CommonActions

---
+++ src/main/java/common/Config.java
@@ -0,0 +1,12 @@
+package common;
+
+public class Config {
+
+    /**
+     * Specify the browser and platform for tests:
+     * CHROME_MAC
+     * FIREFOX_MAC
+     **/
+    public static final String BROWSER_AND_PLATFORM = "CHROME_MAC";
+
+}
