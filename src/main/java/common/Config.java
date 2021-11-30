5f0aac00fa551adcf5ad152d1ef3b633c24a09bf
Vitalii Ismagilov <ismagilov.vel@gmail.com>
2021-11-30 20:33:55 +0200

    BaseTest and Config

--- src/main/java/common/Config.java
+++ src/main/java/common/Config.java
@@ -6,7 +6,16 @@ public class Config {
      * Specify the browser and platform for tests:
      * CHROME_MAC
      * FIREFOX_MAC
-     **/
+     */
     public static final String BROWSER_AND_PLATFORM = "CHROME_MAC";
 
+    /**
+     * Clear browser cookies after each iteration
+     */
+    public static final Boolean CLEAR_COOKIES = true;
+
+    /**
+     * To keep browser open after all scenario/tests
+     */
+    public static final Boolean HOLD_BROWSER_OPEN = true;
 }
