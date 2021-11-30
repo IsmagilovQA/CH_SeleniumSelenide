86134642fc799483ec26874bf3c15c0eb4d7f8c2
Vitalii Ismagilov <ismagilov.vel@gmail.com>
2021-11-30 20:45:33 +0200

    PasePage preparations

--- src/main/java/pages/base/BasePage.java
+++ src/main/java/pages/base/BasePage.java
@@ -1,6 +1,12 @@
 package pages.base;
 
+import constants.Constant;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static constants.Constant.TimeoutVariables.EXPLICIT_WAIT;
 
 public class BasePage {
 
@@ -8,5 +14,23 @@ public class BasePage {
 
     public BasePage(WebDriver driver) {
         this.driver = driver;
+    }
+
+    /**
+     * The method for navigating to specific URL
+     * @param url
+     */
+    public void goToUrl(String url) {
+        driver.get(url);
+    }
+
+    /**
+     * Wait for visibility of element in DOM model
+     * @param element
+     * @return
+     */
+    public WebElement waitElementIsVisible(WebElement element) {
+        new WebDriverWait(driver, EXPLICIT_WAIT).until(ExpectedConditions.visibilityOf(element));
+        return element;
     }
 }
